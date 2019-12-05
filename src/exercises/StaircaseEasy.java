3e1b96b5419a661177703319f18b1b62262e545d
junekinger <junekinger@hotmail.com>
2019-12-04 21:11:00 -0600

    Staircase - easy and better code - comments added

--- src/exercises/StaircaseEasy.java
+++ src/exercises/StaircaseEasy.java
@@ -5,20 +5,12 @@ import java.util.Scanner;
 
 //Pseudocode
 //
-//        I will need a for loop, and within that loop I will need to nest 2 additional for loops.
-//        My plan is to create a blank string that will have spaces added to it first,
-//        then pound signs added second. The loops for spaces and pounds will depend on where
-//        we are in the original loop.
-//
-//        for loop -> goes from 0 to n
-//        variable print_string = ''
-//        for loop -> adds the number of spaces to print_string
-//        for loop -> adds the appropriate number of pounds to print_string
-//        print(print_string)
+//    Complete the staircase function below.
+//    The staircase is actually printed on a 2D array.
+//    If (row index + column index) > n, print “#”; otherwise, print ” “
 
 public class StaircaseEasy {
 
-    /// Complete the staircase function below.
     static void staircase(int n) {
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < n; j++) {
